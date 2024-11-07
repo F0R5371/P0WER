1c2861e4cf0a2c81b4b3970d3fc115e9d636b2bd
4LEX <aidenazavaleta@gmail.com>
2024-11-07 15:36:16 -0600

    Almost finished DFS
    There is still a bug where one node does not get explored.

--- thinker.py
+++ thinker.py
@@ -4,7 +4,7 @@ class Stack:
     def __init__(self, max):
         self.max = max
         self.top = -1
-        self.stack = numpy.empty(max)
+        self.stack = [-1] * max
 
     def push(self, value):
         if self.top == self.max - 1:
@@ -26,9 +26,19 @@ class Stack:
         else:
             return False
         
+    def get_top(self):
+        if self.empty():
+            return
+        
+        return self.stack[self.top]
+
     def print(self):
         for i in range(self.top + 1):
-            print(self.stack[i], end = " ")
+            if (i == self.top):
+                print(self.stack[i], end = " ")
+            else:
+                print(self.stack[i], end = " -> ")
+        print()
 
 class TreeNode:
     def __init__(self, data):
@@ -40,40 +50,42 @@ class Tree:
     def __init__(self, nodes):
         self.nodes = nodes # Representing root as well
         # Instantiates the number of arrays representing every edge between each node
-        self.tree_map = [TreeNode(i) for i in range(nodes)]
+        self.tree_map = [TreeNode(i) for i in range(nodes + 1)]
 
     def add_edge(self, x, *y):
         # Assignment of both to each other makes them linked
-        for ele in range(len(y)):
-            self.tree_map[x].children.append(y[ele])
-            self.tree_map[x].children[ele].parent = x
+        for ele in y:
+            self.tree_map[x].children.append(ele)
+            self.tree_map[ele].parent = x
 
     def dfs_search(self, start, end):
         explored = Stack(self.nodes)
         considered = Stack(self.nodes)
 
-        # Root node already has been explored by default
-        explored.push(0)
+        # If start is 0, then the root node has already been explored
+        explored.push(start)
 
-        def dfs_check(self, )
+        def dfs_check():
+            next = explored.get_top()
+            if next == end:
+                return
+            if len(self.tree_map[next].children) == 0 and considered.empty():
+                return
+            else:
+                for child in self.tree_map[next].children:
+                    # Pushs only the numbers and not a TreeNode class itself
+                    print(child)
+                    considered.push(child)
+                # Pops a node out of considered and explores it
+                explored.push(considered.pop())
+                dfs_check()
+        dfs_check()
 
-        # Going through each node in the tree
-        for i in range(len(self.tree_map)):
-            for j in range(len(explored.stack)):
-                if explored.stack[j] != self.tree_map[i]:
-                    # If the node has children
-                    if len(self.tree_map[i].children) == 0:
-                        break
-                    else:
-                        # Puts each node into the consideration stack
-                        for node in self.tree_map[i].children:
-                            considered.push(self.tree_map[node].data)
-
-                        explored = considered.pop()
-
-        # I'm not completely sure if this works
-        for i in range(len(explored.stack)):
-            print(explored.stack[i])
+        if explored.get_top() != end:
+            print("No solution") 
+        
+        # Return explored
+        explored.print()
     
     def bsf_search(self, start, end):
         pass
@@ -81,10 +93,12 @@ class Tree:
     def print(self):
         pass
     
-brain = Tree(10)
-brain.add_edge(1, 2, 3)
-brain.add_edge(2, 4, 5)
-brain.add_edge(3, 6, 7, 8, 9)
-brain.add_edge(5, 10)
+brain = Tree(11)
+brain.add_edge(0, 1, 2)
+brain.add_edge(1, 3, 4)
+brain.add_edge(2, 5, 6)
+brain.add_edge(3, 7, 8, 9, 10)
+brain.add_edge(5, 11)
 
-brain.dfs_search(0, 0)
+# In the future, work on timing each of these processes and use them for statistics.
+brain.dfs_search(0, 7)
